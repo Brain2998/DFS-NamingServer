29178f577dc0deca4d34e54b59329cf527d36916
brain2998 <an.sereb96@gmail.com>
2018-11-11 15:30:16 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NamingServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
